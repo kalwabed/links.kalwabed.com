042f4c461545a98f48e181ce7bbb1ebf64818b79
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-12-06 22:18:13 +0700

    chore: add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, squooshImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://links.kalwabed.xyz',
   image: {
     service: squooshImageService()
   }
